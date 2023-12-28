1e3fafddcfe943dcef32cfc5eb341eb0fe630e21
Magdiel Vega <mvg140386@gmail.com>
2023-12-28 18:38:18 +0100

    Completando las implementaciones de la capa de percistencia

--- src/main/java/com/inditex/percistence/implementation/ProductDAOImpl.java
+++ src/main/java/com/inditex/percistence/implementation/ProductDAOImpl.java
@@ -28,7 +28,7 @@ public class ProductDAOImpl implements IProductDAO {
 
     @Override
     public List<Product> findByPriceInRange(BigDecimal min, BigDecimal max) {
-        return null;
+        return productRepo.findProductByPriceInRange(min, max);
     }
 
     @Override
--- src/main/java/com/inditex/repository/ProductRepository.java
+++ src/main/java/com/inditex/repository/ProductRepository.java
@@ -1,9 +1,19 @@
 package com.inditex.repository;
 
 import com.inditex.entities.Product;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends CrudRepository<Product, Long> {
+
+    @Query("selec p from Product p WHERE p.price BETWEEN ?1 AND ?2")
+    List<Product> findProductByPriceInRange(BigDecimal minPrice, BigDecimal maxPrice);
+    List<Product> findProductByPriceBetween(BigDecimal minPrice, BigDecimal maxPrice);
+
+
 }
